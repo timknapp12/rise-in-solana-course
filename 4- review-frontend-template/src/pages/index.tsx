dff66097425d81bcdaf37e95edda0fd092dd09d5
Tim Knapp <timknapp12@gmail.com>
2024-07-10 15:06:55 -0600

    add location

--- 4- review-frontend-template/src/pages/index.tsx
+++ 4- review-frontend-template/src/pages/index.tsx
@@ -20,6 +20,7 @@ export default function Home() {
   const [title, setTitle] = useState('');
   const [rating, setRating] = useState(0);
   const [description, setDescription] = useState('');
+  const [location, setLocation] = useState('');
 
   useEffect(() => {
     const fetchAccounts = async () => {
@@ -29,7 +30,7 @@ export default function Home() {
   }, []);
 
   const handleSubmit = () => {
-    const review = new Review(title, rating, description);
+    const review = new Review(title, rating, description, location);
     handleTransactionSubmit(review);
   };
 
@@ -97,6 +98,8 @@ export default function Home() {
           rating={rating}
           setTitle={setTitle}
           setDescription={setDescription}
+          location={location}
+          setLocation={setLocation}
           setRating={setRating}
           handleSubmit={handleSubmit}
         />
